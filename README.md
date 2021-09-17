6cb347718f506b27cf177c8e944872bb91e98c41
muse-sisay <git.muse.sisay@gmail.com>
2021-09-17 18:27:35 +0100

    standardize cli arguments

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ You know the drill, save `login.sh` where ever you want.
 
 ## Setup
 
-It's best to have individual folder for each game. Inside the folder create `GAME` file which contains wargaame title and the ssh port separated by a colon (`:`). Ex. for bandit wargame `bandit:2222`.
+It's best to have individual folder for each game. Inside the folder create `GAME` file which contains wargaame title and the ssh port separated by a colon (`:`). Ex. for bandit wargame `bandit:2220`.
 
 ## Usage
 
@@ -19,15 +19,21 @@ The script has two options, one for saving password another for ssh-ing into the
 ### Save password
 
 ```bash
-$ ./login s 0 bandit0 
+$ ./login -s 0 bandit0 
 LEVEL 0: Trying to login
 ```
 
 ### Login 
 
 ```bash
-$ ./login l 0
+$ ./login -l 0
 LEVEL 0: Saving password
+```
+
+### Save password and try to login 
+
+```console
+$ ./login --save-and-login 0 bandit0
 ```
 
 July 2021, Muse Sisay
--- login.sh
+++ login.sh
@@ -2,23 +2,33 @@
 
 # Read config
 IFS=":"
-while read game port ;do GAME=game PORT=port ; done < GAME
-
-HOST="${GAME}.labs.overthewire.org"
+while read game port ;do WAR_GAME=$game PORT=$port ; done < GAME
+HOST="${WAR_GAME}.labs.overthewire.org"
 
 LEVEL=$2
 
-if [[ $1 = "l" ]] ; then
+function save_passwd(){
+    echo "LEVEL $LEVEL: Saving password"
+    echo "$1" > "${WAR_GAME}${LEVEL}"
+}
+
+function login(){
     echo "LEVEL $LEVEL: Trying to login"
-    if [[ -f "${GAME}${LEVEL}" ]] ; then
-    	sshpass -p $(cat "${GAME}${LEVEL}") ssh "${GAME}${LEVEL}"@$HOST -p $PORT
+    if [[ -f "${WAR_GAME}${LEVEL}" ]] ; then
+    	sshpass -p $(cat "${WAR_GAME}${LEVEL}") ssh "${WAR_GAME}${LEVEL}"@$HOST -p $PORT
     else
 	    echo "Missing password file for level $LEVEL"
         exit 1
     fi
-elif [[ $1 = "s" ]] ; then
-    echo "LEVEL $LEVEL: Saving password"
-    echo "$3" > "${GAME}${LEVEL}"
+}
+
+if [[ $1 = "-l" ]] ; then
+    login $3
+elif [[ $1 = "-s" ]] ; then
+    save_passwd
+elif [[ $1 = "--save-and-login" ]] ; then
+    save_passwd $3
+    login $3
 else
     # Print usage
     echo "Illegal argument $1"
